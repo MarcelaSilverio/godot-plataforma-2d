8beb74b2767b995f807a499fcd3f97cfcfd947e5
Marcela Prata Silvério <marcelapsilverio@gmail.com>
2022-01-17 10:50:52 -0300

    Lesson4

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var move_speed = 400
+var move_speed = 480
 var gravity = 1200
 var jump_force = -720
 var is_grounded
@@ -15,8 +15,11 @@ func _physics_process(delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	
 	is_grounded = _check_is_ground()
+	
+	_set_animation()
 
 func _get_input():
+	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
 	
@@ -34,4 +37,15 @@ func _check_is_ground():
 			
 	return false
 	
+func _set_animation():
+	var anim = "idle"
+	
+	if !is_grounded:
+		anim = "jump"
+		
+	elif velocity.x != 0:
+		anim = "run"
+		
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
 		
