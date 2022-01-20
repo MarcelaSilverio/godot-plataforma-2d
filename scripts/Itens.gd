74082d2b6f56e30b3af1989d35648d04075aecea
Marcela Prata Silvério <marcelapsilverio@gmail.com>
2022-01-20 09:58:14 -0300

    Collected animation working

--- scripts/Itens.gd
+++ scripts/Itens.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
 
-func _ready():
-	pass
+func _on_items_body_entered(body):
+	$anim.play("collected")
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "collected":
+		queue_free()
